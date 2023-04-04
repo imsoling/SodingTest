ec44dbc5bc81c19889821ca1c67af906a74741aa
아임솔이 <101796686+eumsoli@users.noreply.github.com>
2023-04-05 00:06:20 +0900

    [Bronze V] Title: A/B, Time: 224 ms, Memory: 17984 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double a;
+		double b;
+	
+		Scanner scanner = new Scanner(System.in);
+		
+		a = scanner.nextDouble();
+		b = scanner.nextDouble();
+		
+		System.out.println(a/b);
+	}
+
+}
