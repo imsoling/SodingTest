36c1a6862ea607076fba35dbfd7987e52b81cc28
아임솔이 <101796686+eumsoli@users.noreply.github.com>
2023-04-05 00:10:21 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		String id;
+		String str = "??!";
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		id = scanner.next();
+		
+		System.out.println(id+str);
+		
+
+	}
+
+}
