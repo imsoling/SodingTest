d1f36c688519993241e94812a93895e408d666e6
아임솔이 <101796686+eumsoli@users.noreply.github.com>
2023-04-05 00:11:43 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 204 ms, Memory: 17784 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int y;
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		y = scanner.nextInt();
+		
+		System.out.println(y-543);
+		
+		scanner.close();
+
+	}
+
+}
